c96acc32dbf7d626b3f03544d1267b8589095833
Luca <nillability@protonmail.com>
2022-06-10 09:29:28 +0900

    fix warnings

--- src/commands.rs
+++ src/commands.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 
-use teloxide::{prelude::*, utils::command::BotCommands, types::InputFile, RequestError};
-use rand::{Rng, thread_rng};
+use teloxide::{ prelude::*, utils::command::BotCommands, types::InputFile };
+use rand::{ Rng, thread_rng };
 
 mod voices;
 mod auth;
@@ -73,99 +73,11 @@ pub async fn answer(
     Ok(())
 }
 
-// pub async fn answer(
-//     bot: AutoSend<Bot>,
-//     msg: Message,
-// ) -> Result<(), RequestError> {
-//     if msg.text().is_none() {
-//         return respond(());
-//     }
-
-//     match Command::parse(msg.text().unwrap(), "") {
-//         // match command {
-//         Ok(Command::Ping) => {
-//             match msg.from() {
-//                 Some(user) => {
-//                     match &user.username {
-//                         Some(uname) if auth::is_papa(uname) => bot.send_message(msg.chat.id, "pong").await?,
-//                         Some(_) => bot.send_message(msg.chat.id, format!("アーニャおまえのことしらない")).await?,
-//                         None => bot.send_message(msg.chat.id, "アーニャ… わからない").await?,
-//                     }
-//                 }
-//                 None => bot.send_message(msg.chat.id, "アーニャ… わからない").await?,
-//             }
-//         }
-//         Ok(Command::Voice) => {
-//             const VOICES_PATH: &str = "voices";
-//             bot.send_voice(msg.chat.id, InputFile::file(format!("{}/{}", VOICES_PATH, voices::choose_voice()))).await?
-//         }
-//         Ok(Command::Invite) => {
-//             match msg.sender_chat() {
-//                 Some(chat) => {
-//                     match chat.invite_link() {
-//                         Some(link) => bot.send_message(msg.chat.id, format!("りんくそんざいした！\n{}", link)).await?,
-//                         None => bot.send_message(msg.chat.id, "りんくそんざいしない").await?,
-//                     }
-//                 }
-//                 None => bot.send_message(msg.chat.id, "りんくそんざいしない").await?,
-//             }
-//         }
-//         Ok(Command::Bio) => {
-//             match msg.chat.bio() {
-//                 Some(bio) => bot.send_message(msg.chat.id, format!("せつめいそんざいした！\nせつめい： {}", bio)).await?,
-//                 None => bot.send_message(msg.chat.id, "せつめいそんざいしない").await?,
-//             }
-//         }
-//         Ok(Command::Echo(text)) => {
-//             // let command = Command::parse(message.text().unwrap(), "").unwrap();
-//             bot.send_message(msg.chat.id, text).await?
-//         }
-        // _ => {
-        //     let mut rng = thread_rng();
-        //     let roll: bool = rng.gen_bool(1.0 / 10.0);
-        //     if roll {
-        //         bot.send_message(msg.chat.id, quote(msg)).await?;
-        //     } else { return respond(()) }
-        // }
-        // };
-        // }
-        // Error => {
-        //     let mut rng = thread_rng();
-        //     let roll: bool = rng.gen_bool(1.0 / 10.0);
-        //     if roll {
-        //         bot.send_message(msg.chat.id, quote(msg)).await?;
-        //     };
-        //     // respond(());
-        // }
-    // };
-    // respond(())
-
-    // Ok(())
-// }
-
-pub async fn msg_handler(
-    bot: AutoSend<Bot>,
-    msg: Message,
-) -> Result<(), RequestError> {
-    if msg.text().is_none() {
-        return respond(());
-    }
-    let mut rng = thread_rng();
-    let roll: bool = rng.gen_bool(1.0 / 10.0);
-    match roll {
-        true => {
-            bot.send_message(msg.chat.id, quote(msg)).await?;
-        }
-        false => {}
-    };
-    respond(())
-}
-
 fn quote(m: Message) -> String {
     const Q01: &str = "ものすごいうそつき　でも … かっこいいうそつき！";
     const Q02: &str = "だいじょうぶます\nこわくない";
-    const Q03f: &str = "アーニャおうちかえりたい";
-    const Q03l: &str = "とアーニャのおうち";
+    const Q03F: &str = "アーニャおうちかえりたい";
+    const Q03L: &str = "とアーニャのおうち";
     const Q04: &str = "アーニャじゃまなこども？";
 
     let mut rng = thread_rng();
@@ -178,46 +90,11 @@ fn quote(m: Message) -> String {
         1 => Q02.to_string(),
         2 => {
             let name = m.from().unwrap().first_name.as_str();
-            format!("{Q03f}{name}{Q03l}")
+            format!("{Q03F}{name}{Q03L}")
         },
         3 => Q04.to_string(),
         _ => unreachable!(),
     }
-}
-pub async fn ping(
-    b: AutoSend<Bot>,
-    msg: Message,
-    cmd: Command
-) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    match msg.from() {
-        Some(user) => {
-            match &user.username {
-                Some(uname) if auth::is_papa(uname) => b.send_message(msg.chat.id, "pong").await?,
-                Some(_) => b.send_message(msg.chat.id, format!("アーニャおまえのことしらない")).await?,
-                None => b.send_message(msg.chat.id, "アーニャ… わからない").await?,
-            }
-        }
-        None => b.send_message(msg.chat.id, "アーニャ… わからない").await?,
-    };
-    Ok(())
-}
-pub async fn voice(
-    b: AutoSend<Bot>,
-    msg: Message,
-    cmd: Command
-) -> Result<(), Box<dyn Error + Send + Sync>> {
-    const VOICES_PATH: &str = "voices";
-    b.send_voice(msg.chat.id, InputFile::file(format!("{}/{}", VOICES_PATH, voices::choose_voice()))).await?;
-    Ok(())
-}
-pub async fn echo(
-    b: AutoSend<Bot>,
-    msg: Message,
-    cmd: Command,
-    text: String
-) -> Result<(), Box<dyn Error + Send + Sync>> {
-    b.send_message(msg.chat.id, text).await?;
-    Ok(())
 }
 pub async fn res_quote(b: AutoSend<Bot>, m: Message) -> Result<(), Box<dyn Error + Send + Sync>> {
     // let mut rng = thread_rng();
@@ -228,7 +105,7 @@ pub async fn res_quote(b: AutoSend<Bot>, m: Message) -> Result<(), Box<dyn Error
     Ok(())
 }
 
-pub fn roll_dice() -> bool {
-    let mut rng = thread_rng();
-    rng.gen_bool(1.0 / 10.0)
-}
+// pub fn roll_dice() -> bool {
+//     let mut rng = thread_rng();
+//     rng.gen_bool(1.0 / 10.0)
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 // use std::error::Error;
-use teloxide::{prelude::*, utils::command::BotCommands, RequestError};
+use teloxide::{ prelude::* };
 use teloxide::dptree::endpoint;
-
-use dptree::{filter, Handler};
 
 mod commands;
 
@@ -13,37 +11,15 @@ async fn main() {
 
     let bot = Bot::from_env().auto_send();
 
-    // Bot::set_my_commands(bot, commands::Command);
-    // teloxide::commands_repl(bot, commands::answer, commands::Command::ty()).await;
-
-    // teloxide::repl(bot, |message: Message, bot: AutoSend<Bot>| async move {
-    //     bot.send_dice(message.chat.id).await?;
-    //     respond(())
-    // })
-    // .await;
-    // let message_handler(
-    //     bot: 
-    // )
     // let cmd_handler = Update::filter_message().branch(endpoint(commands::answer));
     // let msg_handler = Update::filter_message().branch(endpoint(commands::msg_handler));
     let handler = Update::filter_message()
-        // .branch(dptree::case![commands::Command::Ping]).endpoint(commands::ping)
-        // .branch(dptree::case![commands::Command::Voice]).endpoint(commands::voice)
-        // .branch(dptree::case![commands::Command::Echo(s)]).endpoint(commands::echo);
         .branch(dptree::entry().filter_command::<commands::Command>().endpoint(commands::answer))
         .branch(
             dptree::filter(|msg: Message| msg.text().is_some())
                 // .branch(filter(|| commands::roll_dice())).endpoint(commands::res_quote),
                 .branch(endpoint(commands::res_quote)),
         );
-
-    // let handler = dptree::entry()
-        // .branch(filter_command().endpoint(commands::answer))
-        // .branch(filter_command::<commands::Command, _>).endpoint(commands::answer);
-        // .branch(filter_message()).endpoint(message_handler);
-        // .branch(Update::filter_message()).endpoint(commands::answer);
-        // .branch(cmd_handler)
-        // .branch(msg_handler);
 
     Dispatcher::builder(bot, handler)
         .build()
